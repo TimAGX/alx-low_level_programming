deeaa90336d0ea6cdbb7cd52545755b372ca8ac3
TimAGX <ayomidetimothy28@gmail.com>
2022-03-10 10:15:34 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,5 +2,8 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+	return (0);}
+/**
+ * print programming is like a building a multilingual puzzle
+ * use puts function
+ */
