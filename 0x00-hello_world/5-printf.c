94e30b6aa2d12105af225de01f3fd3a5d44689e3
TimAGX <ayomidetimothy28@gmail.com>
2022-03-10 10:49:47 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/** 
+ * main - print the phrase with proper grammer
+ * Return: 0 
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	Return (0);
+}
