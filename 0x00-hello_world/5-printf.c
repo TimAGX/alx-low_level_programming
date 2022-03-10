6cf2575a0a5e2155a0700f02cd52713cb0268392
TimAGX <ayomidetimothy28@gmail.com>
2022-03-10 10:53:27 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 
 /** 
  * main - print the phrase with proper grammer
- * Return: 0 
+ * Return: 0 if existed properly
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
